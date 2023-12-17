aa8bd4582b9f4c06e7a88d77be080e5720402a93
David Permanov <david.permanov@infobip.com>
2023-12-17 21:49:51 +0100

    Fix cron expression. Fix package-lock.

--- game-session-service/src/main/java/com/permanovd/gamesessionservice/infrastructure/cleanup/DeleteExpiredSessionsJob.java
+++ game-session-service/src/main/java/com/permanovd/gamesessionservice/infrastructure/cleanup/DeleteExpiredSessionsJob.java
@@ -26,7 +26,7 @@ public class DeleteExpiredSessionsJob {
     }
 
     @Transactional
-    @Scheduled(cron = "0 1 * * *")
+    @Scheduled(cron = "0 0 0 * * *")
     public void cleanUp() {
         LocalDateTime dateTime = LocalDateTime.now().minusMonths(1);
         logger.info("Cleaning up expired sessions, ended before {}", dateTime);
